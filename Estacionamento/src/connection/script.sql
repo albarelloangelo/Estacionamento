f92abea1a3ff3bf1a9f6296f1f6040e855fbcb09
02436249059 <02436249059@LAB205-09.fw.iffar.edu.br>
2022-10-27 13:01:35 -0300

    Semana 5 - CRUD Motorista

--- Estacionamento/src/connection/script.sql
+++ Estacionamento/src/connection/script.sql
@@ -7,3 +7,16 @@ CREATE TABLE vaga (
     rua varchar (100) NOT NULL,
     obliqua boolean NOT NULL,
     PRIMARY KEY (idVaga));
+
+
+CREATE TABLE motorista (
+    idMotorista int NOT NULL AUTO_INCREMENT,
+    nome varchar (100) NOT NULL,
+    genero varchar (50) NOT NULL,
+    rg int NOT NULL,
+    cpf int NOT NULL,
+    celular int NOT NULL,
+    email varchar (100) NOT NULL,
+    senha varchar (30) NOT NULL,
+    PRIMARY KEY (idMotorista));
+
